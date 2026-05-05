c256551b3d905a5af1569c9c1d1ace6d936f81e1
Bearofbusiness <dev@bearof.business>
2026-05-04 21:09:56 -0400

    fixed crappy rebase

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 
 pub fn main(init: std.process.Init) !void {
-pub fn main(init: std.process.Init) !void {
     // setup constants
     const allocator = init.arena.allocator();
 
