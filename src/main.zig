86945e2f248a60a56d9484dfdaa216830bfbcb85
Jacob Banta <jacob@banta.email>
2026-09-18 16:17:02 -0400

    compile preprocessed bf

--- src/main.zig
+++ src/main.zig
@@ -79,7 +79,8 @@ pub fn main(init: std.process.Init) !void {
     if (options.preprocessing) {
         const src = try input.allocRemaining(allocator, .unlimited);
         const input_proc_u8: []const u8 = try preprocess.preprocess(allocator, src);
-        _ = input_proc_u8;
+        var input_reader: std.Io.Reader = .fixed(input_proc_u8);
+        try compileBF(asm_output, allocator, &input_reader, options.optimization, options.extensions);
         // finish preprocess impl
     } else {
         try compileBF(asm_output, allocator, input, options.optimization, options.extensions);
